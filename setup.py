90ab4b5ffcb5d79857aabc4419902fb6b6209b9f
AnandBhagat345 <anandbhagat345@.com>
2025-12-25 00:12:50 +0530

    my first message

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'Scrapper',
+    version='0.0.1',
+    author='anand',
+    author_email='anandbhagat345@gmai.com',
+    install_requires=[],
+    packages=find_packages()
+
+
+
+)
