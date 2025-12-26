f0745a694d4ab248ac632ed23f0fecec81481f09
AnandBhagat345 <anandbhagat345@.com>
2025-12-26 22:00:38 +0530

    modify

---
+++ src/utils/__init__.py
@@ -0,0 +1,21 @@
+import sys
+import os
+from src.cloud_io import MongoIo
+from src.constants import MONGO_DATABASE_NAME
+from src.exception import CustomException
+
+
+
+
+
+def fetch_product_from_cloud():
+    try:
+        mongo = MongoIo()
+        collection_names = mongo.mongo_ins._mongo_operation_connect_database.list_collection_name()
+        return [collection_name.replace('_',' ')
+                for collection_name in collection_names]
+
+                
+                
+    except Exception as e:
+        raise CustomException(e,sys)
